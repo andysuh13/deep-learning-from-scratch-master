569de97d556a1eeae6c786440ecc437b0b7788fc
andysuh13 <andysuh13@naver.com>
2023-02-11 16:02:35 +0900

    review_3 추가

--- ch04/review_2.py
+++ ch04/review_2.py
@@ -5,36 +5,36 @@ from dataset.mnist import load_mnist
 from common.functions import sigmoid, softmax, cross_entropy_error
 from common.gradient import numerical_gradient
 
-# (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-# train_size = x_train.shape[0] # 60000
-# batch_size = 10
-# batch_mask = np.random.choice(train_size, batch_size)
-# x_batch = x_train[batch_mask]
-# t_batch = t_train[batch_mask]
+train_size = x_train.shape[0] # 60000
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
 
-# lst = np.arange(5)
-# print(lst) # [0 1 2 3 4]
+lst = np.arange(5)
+print(lst) # [0 1 2 3 4]
 
-# def gradient_descent(f, init_x, lr = 0.01, step_num = 100):
-#     x = init_x
-#     for i in range(step_num):
-#         grad = numerical_gradient(f, x)
-#         x -= lr * grad
-#     return x
+def gradient_descent(f, init_x, lr = 0.01, step_num = 100):
+    x = init_x
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    return x
 
-# class simpleNet:
-#     def __init__(self):
-#         self.W = np.random.randn(2, 3)
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)
 
-#     def predict(self, x):
-#         return np.dot(x, self.W)
+    def predict(self, x):
+        return np.dot(x, self.W)
 
-#     def loss(self, x, t):
-#         z = self.predict(x)
-#         y = softmax(z)
-#         loss = cross_entropy_error(y, t)
-#         return loss
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+        return loss
 
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
