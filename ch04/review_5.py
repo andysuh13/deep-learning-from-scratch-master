850092d692959d793118ef542307e7a0a5d00ed2
andysuh13 <andysuh13@naver.com>
2023-02-11 16:23:27 +0900

    1 에폭당 정확도 계산하는 미니배치 학습 구현 연습

---
+++ ch04/review_5.py
@@ -0,0 +1,49 @@
+# 1 에폭별로 훈련 데이터와 시험 데이터에 대한 정확도 기록
+# 1 에폭 : 학습에서 훈련 데이터를 모두 소진했을 때 횟수
+# 훈련 데이터 10000개를 100개의 미니배치로 학습할 경우, 100회가 1 에폭.
+
+import numpy as np
+from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1) # 60000 / 100 = 600
+
+for i in range(iters_num):
+    # 미니배치 획득
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t.train[batch_mask]
+
+    # 기울기 계산
+    grad = network.numerical_gradient(x_batch, t_batch)
+    # grad = network.gradient(x_batch, t_batch)
+
+    # 매개변수 갱신
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+    
+    # 학습 경과 기록
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    # 1 에폭당 정확도 계산
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc : " + str(train_acc) + "," + str(test_acc))
