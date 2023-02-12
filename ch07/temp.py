ca1c79ed01d90ebd6aa7bd6ee9710dadbecb7d43
andysuh13 <andysuh13@naver.com>
2023-02-12 18:30:28 +0900

    주석 해제

--- ch07/temp.py
+++ ch07/temp.py
@@ -13,10 +13,6 @@ x2 = np.random.rand(10, 3, 7, 7)
 col2 = im2col(x2, 5, 5, stride=1, pad=0)
 print(col2.shape) # (90, 75)
 
-
-
-
-
 class Convolution:
     def __init__(self, W, b, stride = 1, pad = 0):
         self.W = W
@@ -36,23 +32,23 @@ class Convolution:
 
         out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
 
-# class Pooling:
-#     def __init__(self, pool_h, pool_w, stride = 1, pad = 0):
-#         self.pool_h = pool_h
-#         self.pool_w = pool_w
-#         self.stride = stride
-#         self.pad = pad
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride = 1, pad = 0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
 
-#     def forward(self, x):
-#         N, C, H, W = x.shape
-#         out_h = int(1 + (H - self.pool_h) / self.stride)
-#         out_w = int(1 + (W - self.pool_w) / self.stride)
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
 
-#         col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
-#         col = col.reshape(-1, self.pool_h * self.pool_w)
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h * self.pool_w)
 
-#         out = np.max(col, axis = 1)
+        out = np.max(col, axis = 1)
 
-#         out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
 
-#         return out
+        return out
